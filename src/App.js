7d9a2b046046b43aef3bdc395bddf33e2b629904
Manjeet Saini <manjeet29saini@gmail.com>
2023-03-06 04:14:40 +0530

    updating for gh-pages routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
 
 import {
@@ -12,17 +12,15 @@ import {
 
 function App() {
   return (
-    <BrowserRouter>
-      <Box sx={{ backgroundColor: "#000" }}>
-        <Navbar />
-        <Routes>
-          <Route path='/' exact element={<Feed />} />
-          <Route path='/video/:id' element={<VideoDetail />} />
-          <Route path='/channel/:id' element={<ChannelDetail />} />
-          <Route path='/search/:searchTerm' element={<SearchFeed />} />
-        </Routes>
-      </Box>
-    </BrowserRouter>
+    <Box sx={{ backgroundColor: "#000" }}>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact element={<Feed />} />
+        <Route path='/video/:id' element={<VideoDetail />} />
+        <Route path='/channel/:id' element={<ChannelDetail />} />
+        <Route path='/search/:searchTerm' element={<SearchFeed />} />
+      </Routes>
+    </Box>
   );
 }
 

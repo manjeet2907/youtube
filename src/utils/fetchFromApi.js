bc3a1523dd8e420807d37be9738e5981440a02d0
Manjeet Saini <manjeet29saini@gmail.com>
2023-03-06 03:35:39 +0530

    adding changes

---
+++ src/utils/fetchFromApi.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+export const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  params: { maxResults: 50 },
+  headers: {
+    "X-RapidAPI-Key": "51ff9c7400msh198fc955219e459p128421jsnc17f2660c04e",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromApi = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
